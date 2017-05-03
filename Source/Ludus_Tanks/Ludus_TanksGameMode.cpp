5bcd878b5192a0247754ef48b8561f0ebab7e54e
FOX-PC\Fox <a.jeanroy@gludus-academie.com>
2017-05-03 14:28:53 +0200

    landscape/paint/normalmaps/e_battletanks

---
+++ Source/Ludus_Tanks/Ludus_TanksGameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Ludus_Tanks.h"
+#include "Ludus_TanksGameMode.h"
+
+
+
+
---
+++ Source/Ludus_Tanks/Ludus_TanksGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "Ludus_TanksGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LUDUS_TANKS_API ALudus_TanksGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
